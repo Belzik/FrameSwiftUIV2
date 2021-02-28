0e902414e93360c6937f3f44d98ca1b7b14ce2e3
Katrych Alexander <alexander@sheverev.com>
2021-02-28 16:10:25 +0300

    Done

---
+++ FrameSwiftUI/CardsScreen.swift
@@ -0,0 +1,20 @@
+//
+//  FoodScreen.swift
+//  FrameSwiftUI
+//
+//  Created by Александр Катрыч on 28.02.2021.
+//
+
+import SwiftUI
+
+struct FoodScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FoodScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodScreen()
+    }
+}
---
+++ FrameSwiftUI/MoreScreen.swift
@@ -0,0 +1,20 @@
+//
+//  MoreScreen.swift
+//  FrameSwiftUI
+//
+//  Created by Александр Катрыч on 28.02.2021.
+//
+
+import SwiftUI
+
+struct MoreScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoreScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MoreScreen()
+    }
+}
