ea746f245c3c31ad3763c26baa66ed5be2194719
Katrych Alexander <alexander@sheverev.com>
2021-02-28 11:01:14 +0300

    Initial Commit

---
+++ FrameSwiftUI/FrameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameSwiftUIApp.swift
+//  FrameSwiftUI
+//
+//  Created by Александр Катрыч on 28.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct FrameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
